14a365f085b2876c23adec428b8ab3d8952feb26
Yam Yammington <85848371+YamYammington@users.noreply.github.com>
2022-06-15 20:53:29 +0200

    v2.0

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 A bot for Twitch, built in python, that checks for bot accounts in the viewer lists. On detecting a bot, it automatically takes action by banning it or alerting moderators if the bot itself is not one.
 
+To use it you need to install the requests package.
+
 Currently supported commands:  
 
 !help  
 !check
   
+Usage: python twitchbot.py <channel>
--- twitchbot.py
+++ twitchbot.py
@@ -1,24 +1,27 @@
-import sys
+# standard library
+import sys, json, time, socket, re, logging, threading
+
+# needs to be installed
 import requests
-import json
-import time
-import socket
-from threading import Thread
-import re
 
-commands = {
-    '^!check': 'start_banning',
-    '^!help': 'help'
-}
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s][%(levelname)s] %(message)s',
+    handlers=[logging.FileHandler("twitch_bot.log"), logging.StreamHandler(sys.stdout)]
+)
+os.system("color")
+OKCYAN = '\033[96m'
+OK = '\033[92m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
 
 
-# Function for threaded asynchronous function decorator @async
 def _async(func):
     from functools import wraps
 
     @wraps(func)
     def async_func(*args, **kwargs):
-        f = Thread(target=func, args=args, kwargs=kwargs)
+        f = threading.Thread(target=func, args=args, kwargs=kwargs)
         f.start()
         return
     return async_func
@@ -31,43 +34,42 @@ class TwitchBot:
         self.username = username
         self.channel = channel
         self.last_message = 0
+        self.socket_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self):
+        self.read_chat()
 
     def connect_chat(self, delay=2):
-        # create chat connection
         try:
-            print("Connecting to server...")
+            logging.info("Connecting to server...")
             time.sleep(delay)
-            s = socket.socket()
-            s.connect(('irc.chat.twitch.tv', 6667))
-            s.send(f"PASS {self.token}\r\n".encode("utf-8"))
-            s.send(f"NICK {self.username}\r\n".encode("utf-8"))
-            s.send(f"CAP REQ :twitch.tv/membership\r\n".encode("utf-8"))
-            s.send(f"JOIN #{self.channel}\r\n".encode("utf-8"))
-            self.s = s
+            self.socket_conn = socket.socket()
+            self.socket_conn.connect(('irc.chat.twitch.tv', 6667))
+            self.socket_conn.send(f"PASS {self.token}\r\n".encode("utf-8"))
+            self.socket_conn.send(f"NICK {self.username}\r\n".encode("utf-8"))
+            self.socket_conn.send(f"CAP REQ :twitch.tv/membership\r\n".encode("utf-8"))
+            self.socket_conn.send(f"JOIN #{self.channel}\r\n".encode("utf-8"))
+
         except ConnectionResetError as err:
-            print(f"An error occurred:\n{err}\nRetrying...")
+            logging.error(f"{FAIL}An error occurred:\n{err}\nRetrying...{ENDC}")
             self.connect_chat(delay=(delay*2))
 
-    # Send messages to chat
-    def chat(self, sock, msg):
-        sock.send(bytes('PRIVMSG #%s :%s\r\n' % (self.channel, msg), 'UTF-8'))
+    def chat(self, msg):
+        self.socket_conn.send(bytes(f'PRIVMSG #{self.channel} :{msg}\r\n', 'UTF-8'))
 
     @_async
     def read_chat(self):
-        # Starts infinite loop listening to the IRC server
         self.connect_chat()
         while True:
-            response = self.s.recv(1024).decode("utf-8")
+            response = self.socket_conn.recv(1024).decode("utf-8")
             if len(response) == 0:
                 self.connect_chat()
                 continue
 
-            # PONG replies to keep the connection alive
             if response == "PING :tmi.twitch.tv\r\n":
-                self.s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+                self.socket_conn.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
                 continue
 
-            # Separates user and message.
             chat_object = re.search(r'^:(\w+)![^:]+:(.*)$', response)
             if chat_object:
                 self.process_chat(chat_object)
@@ -76,31 +78,27 @@ class TwitchBot:
     def process_chat(self, chat_object):
         user = chat_object.group(1)
         msg = chat_object.group(2)
-        for command, action in commands.items():
-            if bool(re.match(command, msg)):
-                if action == 'help':
-                    print(f"{user} issued command: HELP")
-                    self.chat(self.s, f'Hi {user}! I am BotChecker. For more information, go to https://github.com/YamYammington/twitchbotchecker')
+        commands = [('^!help$', 'help'), ('^!check$', 'check')]
+        for regex, keyword in commands.items():
+            if re.match(regex, msg):
+                if keyword == 'help':
+                    logging.info(f"{user} issued command: HELP")
+                    self.chat(f'Hi {user}! I am BotChecker. For more information, go to https://github.com/YamYammington/twitchbotchecker')
                     return
-                elif action == 'start_banning':
+                elif keyword == 'check':
                     if user not in self.get_viewers()[1]:
-                        self.chat(self.s, "Sorry, you need to be a moderator to use this command!")
+                        self.chat("Sorry, you need to be a moderator to use this command!")
                         return
-                    print(f"{user} issued command: CHECK")
-                    self.chat(self.s, "Beginning search...")
+                    logging.info(f"{user} issued command: CHECK")
+                    self.chat("Beginning search...")
                     self.check_for_bots()
                     return
-                else:
-                    print(f"No command detected in message: {msg}.")
-                    continue
-            else:
-                continue
 
     def get_viewers(self):
-        r = requests.get(f"http://tmi.twitch.tv/group/user/{self.channel}/chatters")
-        converted = json.loads(r.text)
-        moderator_list = converted['chatters']['moderators'] + converted['chatters']['broadcaster'] + converted['chatters']['vips'] + converted['chatters']['admins'] + converted['chatters']['staff'] + converted['chatters']['global_mods']
-        return converted['chatters']['viewers'], moderator_list
+        raw_json = requests.get(f"http://tmi.twitch.tv/group/user/{self.channel}/chatters").text
+        bot_dict = json.loads(raw_json)
+        moderator_list = bot_dict['chatters']['moderators'] + bot_dict['chatters']['broadcaster'] + bot_dict['chatters']['vips'] + bot_dict['chatters']['admins'] + bot_dict['chatters']['staff'] + bot_dict['chatters']['global_mods']
+        return bot_dict['chatters']['viewers'], moderator_list
 
     def get_moderator_status(self):
         if "botchecker_yam" in self.get_viewers()[1]:
@@ -111,58 +109,50 @@ class TwitchBot:
     def check_for_bots(self):
         bot_count = 0
         mod_status = self.get_moderator_status()
-        text = "Bot is a moderator." if mod_status else "Bot is not a moderator."
-        print(text)
         viewerlist = self.get_viewers()[0]
         botlist = get_bot_accounts()
         if not viewerlist:
-            self.chat(self.s, "There are no viewers!")
+            logging.info("There are no viewers.")
+            self.chat("There are no viewers.")
             return
         for user in viewerlist:
             if user in botlist:
                 if mod_status:
                     bot_count += 1
-                    print(f"Bot found: {user}")
-                    self.chat(self.s, f"/ban {user}")
+                    logging.info(f"{OKCYAN}Bot found: {user}{ENDC}")
+                    self.chat(f"/ban {user}")
                 else:
                     bot_count += 1
-                    print(f"Bot found: {user}")
-                    self.chat(self.s, f"User {user} is a bot!")
-            else:
-                continue
+                    logging.info(f"{OKCYAN}Bot found: {user}{ENDC}")
+                    self.chat(f"User {user} is a bot!")
 
         if bot_count == 0:
-            self.chat(self.s, "No bots detected at the moment.")
+            self.chat("No bots detected at the moment.")
         else:
             if mod_status:
-                self.chat(self.s, f"Finished checking, banned {bot_count} bots.")
-                return
+                self.chat(f"Finished checking, banned {bot_count} bots.")
             else:
-                self.chat(self.s, f"Finished checking, found {bot_count} bots.")
+                self.chat(f"Finished checking, found {bot_count} bots.")
 
 
 def get_bot_accounts():
     bot_list = []
     r = requests.get("https://api.twitchinsights.net/v1/bots/all")
     converted = json.loads(r.text)
-    for bot in converted["bots"]:
-        bot_list.append(bot[0])
-    print(f"API returned {len(bot_list)} bot accounts.")
+    for account in converted["bots"]:
+        bot_list.append(account[0])
+    logging.debug(f"API returned {len(bot_list)} bot accounts.")
     return bot_list
 
 
-def main():
+if __name__ == "__main__":
     username = "botchecker_yam"
     client_id = "XXXXX"
     token = "oauth:XXXXX"
     channel = sys.argv[1]
 
     bot = TwitchBot(username, client_id, token, channel)
-    bot.read_chat()
-    time.sleep(7)
-    bot.chat(bot.s, 'BotChecker is now online. !help for information.')
-    print("Bot online!")
-
-
-if __name__ == "__main__":
-    main()
+    bot.start()
+    bot.chat('BotChecker is now online. !help for information.')
+    logging.info("Bot online!")
+    logging.info("Bot is a moderator." if bot.get_moderator_status() else "Bot is not a moderator.")
